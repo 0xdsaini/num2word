727f84460611704a5ab3a49b23037f5e4d7ad596
Devesh <futuredevesh@gmail.com>
2015-01-02 17:29:45 +0530

    License text added in "main.py"

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2014 Devesh Saini(futuredevesh@gmail.com).
+#
+# This file is part of num2word.
+#
+#     num2word is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     num2word is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with num2word.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Integers to Verbal numbers converter i.e. 100 to 'One Hundred'"""
+
 class verbal_num(object):
 
     def __init__(self, num):
@@ -83,7 +102,7 @@ class verbal_num(object):
 
             return verbnum
 
-        else:
+        else: #Conversion from 1000 to infinity is done here.
 
             verbnum = ""
 
