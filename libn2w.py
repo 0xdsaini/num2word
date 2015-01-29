b01a41862b0ac3ad67d2ef07dbeb087caabf6889
Devesh <futuredevesh@gmail.com>
2015-01-29 14:40:17 +0530

    Bug Fix : Removed comma with Hundred since it is not a cofficient of 10^3

--- libn2w.py
+++ libn2w.py
@@ -54,7 +54,7 @@ class Verbal(object):
         self.data_ones = {"0":"", "1":"One", "2":"Two", "3":"Three", "4":"Four", "5":"Five", "6":"Six", "7":"Seven", "8":"Eight", "9":"Nine"}
         self.data_tens = {"10":"Ten", "11":"Eleven", "12":"Twelve", "13":"Thirteen", "14":"Fourteen", "15":"Fifteen", "16":"Sixteen", "17":"Seventeen", "18":"Eighteen", "19":"Nineteen", "2":"Twenty", "3":"Thirty", "4":"Fourty", "5":"Fifty", "6":"Sixty", "7":"Seventy", "8":"Eighty", "9":"Ninety"}
 
-        self.data_hundreds = "Hundred" + ","
+        self.data_hundreds = "Hundred"
         self.data_thousands = "Thousand" + ","
         self.data_millions = "Million" + ","
 
