ca68c805f3b5ece81b54e68d6b7ea8f61f945b3b
Devesh <futuredevesh@gmail.com>
2015-02-01 19:49:56 +0530

    Adding a setup.py file to make it installable library named "libn2w".

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(name="num2word",
+                 version="1.0",
+                 description="Library to convert numbers to english words",
+                 author="Devesh Saini",
+                 author_email="futuredevesh@gmail.com",
+                 license="GNU GPLv2",
+                 url="https://github.com/devesh525s/num2word",
+                 packages=setuptools.find_packages())
